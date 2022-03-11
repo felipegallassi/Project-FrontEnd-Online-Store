ddfe005fcffe22b90299f0e183231795092c9a18
Felipe Gallassi <felipegallassi@gmail.com>
2022-03-11 16:29:45 -0300

    requisito1 ok

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const promise = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const response = await fetch(promise);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return new Error(error);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
+    const promise = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    const response = await fetch(promise);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return new Error(error);
+  }
 }
