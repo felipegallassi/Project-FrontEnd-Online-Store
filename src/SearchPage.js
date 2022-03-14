3a24bb8946e6a67090127234be3f8362fc2935cf
Felipe Gallassi <felipegallassi@gmail.com>
2022-03-14 17:17:13 -0300

    requisito 4 OK

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { getCategories } from './services/api';
 
 class SearchPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const categories = await getCategories();
+    this.setState({ categories });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div className="search">
         <input className="search-input" />
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        <div className="sideBar">
+          {categories.map((categoria) => (
+            <label htmlFor={ categoria.id } data-testid="category" key={ categoria.id }>
+              <input
+                type="radio"
+                id={ categoria.id }
+                name={ categoria.id }
+                value={ categoria.name }
+              />
+              {categoria.name}
+            </label>
+          ))}
+        </div>
       </div>
     );
   }
